37c05385e71bdf0c2270a4cf2ce246a36e47954b
petrovska4 <tijanapetrovska173@gmail.com>
2023-11-13 13:08:12 +0100

    v2.4

--- fragrances/si.js
+++ fragrances/si.js
@@ -1,4 +1,7 @@
 let likesF = JSON.parse(localStorage.getItem('likesF')) || 0;
+let price = localStorage.getItem('priceSi') || '220';
+var reviewsCount = 0;
+var selectedValue;
 
 function like() {
   likesF += 1;
@@ -7,26 +10,96 @@ function like() {
 }
 
 function changePrice() {
-  var size = document.getElementById('size').value;
-  if (size == "30ml") {
+  selectedValue = document.getElementById('sizeSi').value;
+  if (selectedValue == "30ml") {
     document.getElementById('price').innerText = "USD 220.00";
-  } else if (size == "50ml") {
+    localStorage.setItem('priceSi', '220');
+  } else if (selectedValue == "50ml") {
     document.getElementById('price').innerText = "USD 360.00";
+    localStorage.setItem('priceSi', '360');
   } else {
     document.getElementById('price').innerText = "USD 450.00";
+    localStorage.setItem('priceSi', '450');
   }
 }
 
 function addReview(e) {
   const tag = document.createElement("p");
   const text = document.createTextNode(e.target.value);
+  const date = new Date().toLocaleDateString([], { hour: '2-digit', minute: '2-digit' });
+  tag.appendChild(document.createTextNode("Date: " + date));
+  tag.appendChild(document.createElement('br'));
   tag.appendChild(text);
-  document.getElementById('reviews').appendChild(tag);
+
+  const div = document.createElement("div");
+  div.style.margin = "10px 0px 0px 0px";
+  div.appendChild(tag);
+
+  reviewsCount += 1;
+  
+  document.getElementById('reviews').appendChild(div);
+
+  document.getElementById('reviewsCount').innerText = "Cutomer reviews (" + reviewsCount + ")"; 
+
+  document.getElementById('addReview').value = '';
 }
+
+function addItem() {
+  var tag = document.createElement('div');
+
+  var img = document.createElement('img');
+  img.src = "fragrances1.jpg";
+  img.style.height = "150px";
+  img.style.width = "auto";
+
+  var priceSi = localStorage.getItem('priceSi');
+
+  var text1 = document.createTextNode("Smells like winter spirit");
+  var text2 = document.createTextNode("Price: " + priceSi + ".00");
+  var text3 = document.createTextNode("Size: " + selectedValue);
+
+  var br1 = document.createElement('br');
+  var br2 = document.createElement('br');
+  var br3 = document.createElement('br');
+
+  var exit = document.createElement('button');
+  exit.appendChild(document.createTextNode("X"));
+  
+  tag.appendChild(img);
+  tag.appendChild(br3);
+  tag.appendChild(text1);
+  tag.appendChild(exit);
+  tag.appendChild(br1);
+  tag.appendChild(text2);
+  tag.appendChild(br2);
+  tag.appendChild(text3);
+
+  document.getElementById('addHere').appendChild(tag);
+  showCartDialog();
+
+  exit.addEventListener('click', function() {
+    tag.remove();
+  });
+}
+
+function showCartDialog() {
+  document.getElementById('cartSi').showModal();
+}
+function closeCartDialog() {
+  document.getElementById('cartSi').close();
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('likeCount').innerText = "Likes: " + likesF;
-  document.getElementById('price').innerText = "USD 220.0";
+  document.getElementById('price').innerText = "USD 220.00";
+  document.getElementById('reviewsCount').innerText = "Cutomer reviews (" + reviewsCount + ")";
+
+  document.getElementById('sizeSi').addEventListener('change', changePrice);
+  document.getElementById('addSi').addEventListener('click', addItem);
+  document.getElementById('like').addEventListener('click', like);
+  document.getElementById('buyNow').addEventListener('click', closeCartDialog);
+
   const input = document.getElementById("addReview");
   input.addEventListener("keydown", function(e) {
     if ( e.key == 'Enter' ) {
---
+++ logIn.js
@@ -0,0 +1,59 @@
+let name = localStorage.getItem('name') || "Tijana";
+let flag = localStorage.getItem('flag') || "false";
+let email = localStorage.getItem('email') || "tijanapetrovska@gmail.com";
+let password = localStorage.getItem('password') || "1234";
+
+function showDialog() {
+  document.getElementById("signInDialog").showModal();
+}
+function showRegisterDialog() {
+  document.getElementById("registerDialog").showModal();
+}
+function closeDialog() {
+  document.getElementById("signInDialog").close();
+}
+function closeRegisterDialog() {
+  document.getElementById("registerDialog").close();
+  showDialog();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var signInForm = document.getElementById('signInForm');
+  var registerForm = document.getElementById('registerForm');
+
+  if( flag === "true" ) {
+    document.getElementById('user').innerText = "Welcome " + name;
+  }
+  
+  function loginRegister() {
+    var popup = document.getElementById("loginPopup");
+    popup.classList.toggle("show");
+  }  
+  
+  function submit() {
+    var emailInput = document.getElementById('email').value;
+    var passwordInput = document.getElementById('password').value;
+    if( localStorage.getItem('email') === emailInput && localStorage.getItem('password') === passwordInput ) {
+      closeDialog();
+      document.getElementById('user').innerText = "Welcome " + localStorage.getItem('name');
+      localStorage.setItem('flag', "true");
+    } else {
+      alert("Wrong email or password!");
+    }
+  }
+
+  function register() {
+    var emailInput = document.getElementById('emailRegister').value;
+    var passwordInput = document.getElementById('passwordRegister').value;
+    var name = document.getElementById('name').value;
+    localStorage.setItem('email', emailInput);
+    localStorage.setItem('password', passwordInput);
+    localStorage.setItem('name', name);
+    closeRegisterDialog();
+  }
+  
+  document.getElementById('login/register').addEventListener('click', loginRegister);
+  signInForm.addEventListener('click', submit);
+  registerForm.addEventListener('click', register);
+});
